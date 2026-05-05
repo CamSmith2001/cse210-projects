c1f6bd3b1327a495e5c49f6a5567e81aea2c91a9
CamSmith2001 <Cam.weston23@gmail.com>
2026-05-05 12:44:46 -0600

    Completed Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Northrop";
+        job1._startYear = 2001;
+        job1._endYear = 2026;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Truck Washer";
+        job2._company = "Modern Maintainence";
+        job2._startYear = 2017;
+        job2._endYear = 2026;
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Cam Smith";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+        
+
     }
 }
